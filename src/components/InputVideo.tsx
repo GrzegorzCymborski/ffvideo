731b99b72463f0273e4c46070089152169e41c8d
Grzegorz <cymborski@gmail.com>
2020-12-05 17:40:08 +0100

    add upload icon

--- src/components/InputVideo.tsx
+++ src/components/InputVideo.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { useFFmpeg } from "../context/FFmpegContext";
+import { ReactComponent as UploadIcon } from "../assets/uploadIcon.svg";
 
 const InputVideo = () => {
   const { setVideo } = useFFmpeg();
   return (
-    <input
-      type="file"
-      accept="video/*"
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-        setVideo(e.target.files?.item(0))
-      }
-    />
+    <>
+      <label htmlFor="fileUpload">
+        <div style={{ width: "150px", height: "150px", cursor: "pointer" }}>
+          <UploadIcon fill="#943434" />
+        </div>
+      </label>
+      <input
+        style={{ display: "none" }}
+        type="file"
+        accept="video/*"
+        id="fileUpload"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVideo(e.target.files?.item(0))}
+      />
+    </>
   );
 };
 
